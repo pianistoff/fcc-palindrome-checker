53731983bf8b8aa9bb4e38f484f57121b30a8043
Severyn Hundiak <severyn.h@gmail.com>
2022-01-31 21:58:53 +0200

    Make basic markup for palindrome app

---
+++ script.js
@@ -0,0 +1,23 @@
+function palindrome(str) {
+    const converted = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
+    const arr = [];
+    converted.split('').forEach(letter => arr.unshift(letter));
+    if (converted === arr.join('')) {
+      return true;
+    } else {
+      return false
+    }
+  }
+
+const str = document.getElementById("palindrome").value;
+const result = document.getElementById("result");
+
+function display(str) {
+    if (palindrome(str)) {
+        result.textContent = "This is a palindrome!";
+        result.style.color = "green";
+    } else {
+        result.textContent = "This is not a palindrome!";
+        result.style.color = "red";
+    }
+}
